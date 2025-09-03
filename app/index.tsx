3166716f7a843f238893e322a22f68a2815ae440
alexbylegend03 <alexbylegend03@gmail.com>
2025-09-03 21:19:17 +0000

    💄

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,6 @@
 import { Text, View } from "react-native";
+import { WelcomeText } from "@/components/WelcomeText";
+import { Welcomepepe } from "@/components/WelcomeText";
 
 export default function Index() {
   return (
@@ -9,7 +11,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <WelcomeText></WelcomeText>
+      <Welcomepepe></Welcomepepe>
     </View>
   );
 }
---
+++ components/WelcomeText.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+export function WelcomeText():any{
+    return <Text>Hola Mundo</Text>
+}export function Welcomepepe():any{
+    return <Text>Hola pepe</Text>
+}
